c39b8e078ebb11ac2b44a5be8153d647beb8d926
Lucas <lucashravazi55@gmail.com>
2025-09-29 18:00:16 -0300

    defining auth controller structure

--- RifaBox.API/Controllers/AuthController.cs
+++ RifaBox.API/Controllers/AuthController.cs
@@ -1,26 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
+using RifaBox.Domain.Entities;
 using RifaBox.Infra.DataBase.Repositories;
-
-using Microsoft.AspNetCore.Mvc;
-
-/*
-Controller de autenticação
-ENDPOINTS
-    POST /api/auth/register - Registrar usuário
-    POST /api/auth/login - Login e obter token jwt
-    GET /api/auth/me - Obter info usuário atual
-*/
 
 namespace RifaBox.API.Controllers
 {
-    [ApiController]
+    [ApiController] // controller de autenticação
     [Route("api/[controller]")]
     public class AuthController : ControllerBase
     {
 
+        [HttpPost("register")] // POST /api/auth/register - Registrar usuário
+        public async Task<IActionResult> Register([FromBody] RegisterDTO registerDto)
+        {
+
+        }
+
+        [HttpPost("login")] //POST /api/auth/login - Login e obter token jwt
+        public async Task<IActionResult> Login([FromBody] LoginDTO loginDto)
+        {
+
+        }
+
+        [HttpGet("me")] //GET /api/auth/me - Obter info usuário atual
+        public IActionResult GetCurrentUserInfo()
+        {
+
+        }
+
+        pivate string JwtTokenGenerator(User user)
+        {
+            // lógica para geração de token
+        }
     }
 }

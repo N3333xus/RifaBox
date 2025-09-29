0fa5cf5801017a0f1a90c02668da59a63275106a
Lucas <lucashravazi55@gmail.com>
2025-09-29 09:53:48 -0300

    infra repositories inheritance from domain Interfaces & Domain.csproj ProjectReference on Infra.csproj

--- RifaBox.Infra/DataBase/Repositories/TicketRepository.cs
+++ RifaBox.Infra/DataBase/Repositories/TicketRepository.cs
@@ -2,10 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using RifaBox.Domain.Interfaces.Repositories;
 
 namespace RifaBox.Infra.DataBase.Repositories
 {
-    public class TicketRepository
+    public class TicketRepository: ITicketRepository
     {
         
     }
